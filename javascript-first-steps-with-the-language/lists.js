44a58ffb49b33a85a6a91934edfa308ebfe48345
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-04 17:19:41 -0300

    javascript-first-steps: Creating lists

---
+++ javascript-first-steps-with-the-language/lists.js
@@ -0,0 +1,17 @@
+console.log("Work with lists");
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const destinationList = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+destinationList.push(`Curitiba`)//adding an item to the list
+
+console.log("Possible destinations");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(destinationList);
--- javascript-first-steps-with-the-language/variableAssignment.js
+++ javascript-first-steps-with-the-language/variableAssignment.js
@@ -1,6 +1,5 @@
 console.log("Work with variable assignment");
 
-const age = 35;
 const firstName = "Wagner";
 const lastName = "Oliveira";
 
@@ -13,3 +12,8 @@ counter = counter + 1;
 
 const fullName = firstName + lastName;
 console.log(fullName);
+
+let age; //declaring variable
+age = 35; //assigning value
+age = age + 1;
+console.log(age);
