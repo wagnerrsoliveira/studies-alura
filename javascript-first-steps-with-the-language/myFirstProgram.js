7badb46daa4cb1c57dc46b0e5e4f44e4f52e4413
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-04 16:14:58 -0300

    javascript-first-steps: Good practices with variables

---
+++ javascript-first-steps-with-the-language/myFirstProgram.js
@@ -0,0 +1,12 @@
+console.log("My first program work with variable");
+
+const age = 26;
+
+console.log("age")
+console.log(age);
+console.log(age + 2);
+console.log(age - 2);
+console.log(age / 2);
+
+const ageSum = age + 2;
+console.log(ageSum)
---
+++ javascript-first-steps-with-the-language/workWithVariable.js
@@ -0,0 +1,5 @@
+console.log("Work with variable");
+//JS is case sensitive
+
+const age = 29;
+const name = "Wagner";
