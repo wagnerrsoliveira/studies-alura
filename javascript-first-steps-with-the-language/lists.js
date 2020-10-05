65a3a179918d201efe0c82cea1e7d60a11162f12
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-04 23:17:22 -0300

    javascript-first-steps: Removing element

--- javascript-first-steps-with-the-language/lists.js
+++ javascript-first-steps-with-the-language/lists.js
@@ -15,3 +15,7 @@ destinationList.push(`Curitiba`)//adding an item to the list
 console.log("Possible destinations");
 // console.log(salvador, saoPaulo, rioDeJaneiro);
 console.log(destinationList);
+
+destinationList.splice(1,1);
+console.log(destinationList);
+console.log(destinationList[1],destinationList[0]);
