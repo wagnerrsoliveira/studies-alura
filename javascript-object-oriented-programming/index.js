245db06fe7357fa35651366c88e81ef37661b44e
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-24 10:33:38 -0300

    javascript-object-oriented-programming: Understanding the customer's problem

---
+++ javascript-object-oriented-programming/index.js
@@ -0,0 +1,11 @@
+const customer1Name =  "Wagner";
+const customer1Document = 11122233309;
+const customer1Agency = 1001;
+const customer1Balance = 0;
+
+const customer2Name =  "Roberto";
+const customer2Document = 88822233309;
+const customer2Agency = 1001;
+const customer2Balance = 0;
+
+console.log(customer1Name);
