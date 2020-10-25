ec05cf81526f0fe390c74b042bc643a5040a3a41
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-25 07:53:03 -0300

    javascript-object-oriented-programming: Class Behaviors

--- javascript-object-oriented-programming/index.js
+++ javascript-object-oriented-programming/index.js
@@ -1,21 +1,40 @@
 class Customer {
     name;
     document;
-    agency;
-    balance;
 }
 
+class CurrentAccount {
+    agency;
+    balance;
+
+    withdrawm(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        }
+    }
+}
+
+
 const customer1 = new Customer();
-const customer2 = new Customer();
-
-customer1.name =  "Wagner";
+customer1.name = "Wagner";
 customer1.document = 11122233309;
-customer1.agency = 1001;
-customer1.balance = 0;
 
-customer2.name =  "Roberto";
+const customer2 = new Customer();
+customer2.name = "Roberto";
 customer2.document = 88822233309;
-customer2.agency = 1001;
-customer2.balance = 0;
 
-console.log(customer1,customer2);
+const currentAccountWagner = new CurrentAccount();
+currentAccountWagner.balance = 0;
+currentAccountWagner.agency = 1001;
+console.log(currentAccountWagner.balance);
+
+currentAccountWagner.balance = 100;
+console.log(currentAccountWagner.balance);
+
+currentAccountWagner.withdrawm(50);
+console.log(currentAccountWagner.balance);
+
+
+console.log(customer1);
+console.log(customer2);
+
