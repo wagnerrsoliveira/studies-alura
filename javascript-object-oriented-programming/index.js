02f8a2b29e42b90aa77ccfc390a6153893833090
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-26 06:59:31 -0300

    javascript-object-oriented-programming: Null and undefined

--- javascript-object-oriented-programming/index.js
+++ javascript-object-oriented-programming/index.js
@@ -5,9 +5,9 @@ const customer1 = new Customer();
 customer1.name = "Wagner";
 customer1.document = 11122233309;
 
-const customer2 = new Customer();
-customer2.name = "Roberto";
-customer2.document = 88822233309;
+//const customer2 = new Customer();
+//customer2.name = "Roberto";
+//customer2.document = 88822233309;
 
 const currentAccountWagner = new CurrentAccount();
 currentAccountWagner.agency = 1001;
@@ -16,7 +16,9 @@ currentAccountWagner.deposit(500);
 
 const currentAccountRoberto = new CurrentAccount();
 currentAccountRoberto.agency = 1002;
-currentAccountRoberto.customer = customer2;
+currentAccountRoberto.customer = new Customer();
+currentAccountRoberto.customer.name ="Roberto";
+currentAccountRoberto.customer.document = 88822233309;
 
 let amount = 200;
 currentAccountWagner.transfer(amount,currentAccountRoberto)
