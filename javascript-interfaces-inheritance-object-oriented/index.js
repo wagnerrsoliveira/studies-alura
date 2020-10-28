aa50edd48feedefcf7f81d1547e106be8ec3bf81
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-10-28 06:23:22 -0300

    javascript-interfaces-inheritance-object-oriented: Sharing code

--- javascript-interfaces-inheritance-object-oriented/index.js
+++ javascript-interfaces-inheritance-object-oriented/index.js
@@ -1,14 +1,15 @@
 import { Customer } from './Customer.js';
 import { CurrentAccount } from './CurrentAccount.js';
 import { SavingsAccount } from './SavingsAccount.js';
+import { Account } from './Account.js';
 
 const customer1 = new Customer("Wagner",11122233309);
 
-const currentAccountWagner = new CurrentAccount(customer1,1001);
+const currentAccountWagner = new Account(50, customer1,1001);
 currentAccountWagner.deposit(500);
 currentAccountWagner.withdrawm(100);
 
-const savingsAccountWagner = new SavingsAccount(50,customer1, 1001);
+const savingsAccountWagner = new Account(50,customer1, 1001);
 
 console.log(savingsAccountWagner);
 console.log(currentAccountWagner);
