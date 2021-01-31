200c499b4c69e5d625bc36780482c38ce1195633
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-01-31 10:02:11 -0300

    asp.net-core-a-webapp-using-mvc: a response default

--- asp.net-core-a-webapp-using-mvc/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ asp.net-core-a-webapp-using-mvc/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,9 +1,21 @@
+using System.Threading.Tasks;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void Configure(){
-            
+        public void Configure(IApplicationBuilder app)
+        {
+             app.Run(BooksToRead);
+        }
+
+        public Task BooksToRead(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
     }
 }
