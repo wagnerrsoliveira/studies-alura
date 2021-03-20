ad94f3ccf0429f5b20ecc0ea7682fd8fc5207f73
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-20 08:21:32 -0300

    rest-nodejs-api-with-express-and-mysql: create attendances table

--- rest-nodejs-api-with-express-and-mysql/index.js
+++ rest-nodejs-api-with-express-and-mysql/index.js
@@ -1,11 +1,14 @@
 const customExpress = require('./config/customExpress');
 const connection = require('./infrastructure/connection');
+const Tables = require('./infrastructure/tables');
 
 connection.connect(error => {
     if(error){
         console.log(error);
     }else{
         console.log("database connected");
+
+        Tables.init(connection);
         const app = customExpress();
 
         app.listen(3000,() => console.log("server run at port 3000"));
