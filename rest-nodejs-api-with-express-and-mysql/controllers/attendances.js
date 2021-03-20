55e82b2e9669eaf33237ef15b12364c916af12d7
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-20 07:22:50 -0300

    rest-nodejs-api-with-express-and-mysql: create attendance POST route

--- rest-nodejs-api-with-express-and-mysql/controllers/attendances.js
+++ rest-nodejs-api-with-express-and-mysql/controllers/attendances.js
@@ -1,3 +1,5 @@
 module.exports= app => {
-    app.get('/attendance',(req, res) => res.send("Your are on attendance route, and calling a GET") )
+    app.get('/attendance',(req, res) => res.send("Your are on attendance route, and calling a GET") );
+
+    app.post('/attendance', (req, res) => res.send("Your are on attendance route, and calling a POST") );
 }
