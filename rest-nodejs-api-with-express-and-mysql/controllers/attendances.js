015a1246da470458c37a2d56762a5369164cd156
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-20 07:31:48 -0300

    rest-nodejs-api-with-express-and-mysql: send json and urlencoded data

--- rest-nodejs-api-with-express-and-mysql/controllers/attendances.js
+++ rest-nodejs-api-with-express-and-mysql/controllers/attendances.js
@@ -1,5 +1,10 @@
 module.exports= app => {
     app.get('/attendance',(req, res) => res.send("Your are on attendance route, and calling a GET") );
 
-    app.post('/attendance', (req, res) => res.send("Your are on attendance route, and calling a POST") );
+    app.post('/attendance', (req, res) => {
+        console.log(req.body);
+
+        res.send("Your are on attendance route, and calling a POST")
+        
+    } );
 }
