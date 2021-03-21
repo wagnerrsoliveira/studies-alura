4d9b7af20bea9d1e17b6cfa1af6e842f071ffd2a
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-21 07:17:55 -0300

    rest-nodejs-api-with-express-and-mysql: add attendance dates fields

--- rest-nodejs-api-with-express-and-mysql/models/attendances.js
+++ rest-nodejs-api-with-express-and-mysql/models/attendances.js
@@ -1,11 +1,17 @@
+const moment = require('moment');
 const connection = require('../infrastructure/connection');
 
 class Attendance{
 
     add(attendance){
+
+        const dateCreate = moment().format('YYYY-MM-DD HH:MM:SS');
+        const date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        const attendanceDated = {...attendance, dateCreate, date};
+
         const sql = 'INSERT INTO attendances SET ?';
 
-        connection.query(sql, attendance, (error, result) =>{
+        connection.query(sql, attendanceDated, (error, result) =>{
             if(error){
                 console.log(error)
             }else{
