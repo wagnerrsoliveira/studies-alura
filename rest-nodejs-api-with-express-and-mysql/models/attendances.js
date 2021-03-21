fc8255cacf647590ecda8ac040d35aef37e47859
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-03-21 07:39:11 -0300

    rest-nodejs-api-with-express-and-mysql: add validation to the input data

--- rest-nodejs-api-with-express-and-mysql/models/attendances.js
+++ rest-nodejs-api-with-express-and-mysql/models/attendances.js
@@ -7,18 +7,42 @@ class Attendance{
 
         const dateCreate = moment().format('YYYY-MM-DD HH:MM:SS');
         const date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const dateIsValid = moment(date).isSameOrAfter(dateCreate);
+        
         const attendanceDated = {...attendance, dateCreate, date};
+        const customerIsValid= attendance.customer.length >= 5;
 
-        const sql = 'INSERT INTO attendances SET ?';
+        const validations = [
+            { 
+                name: 'date', 
+                isValid: dateIsValid,
+                message:'Date must to be greater than or equal to the current date'
+            },
+            { 
+                name: 'customer', 
+                isValid: customerIsValid,
+                message:'Customer must have more than five characters'
+            },
+        ];
 
-        connection.query(sql, attendanceDated, (error, result) =>{
-            if(error){
-                res.status(400).json(error)
-            }else{
-                res.status(201).json(result)
-            }
+        const errors = validations.filter(field => !field.isValid);
+        const hasErrors = errors.length;
 
-        })
+        if(hasErrors){
+            res.status(400).json(errors)
+        }else{
+            const sql = 'INSERT INTO attendances SET ?';
+    
+            connection.query(sql, attendanceDated, (error, result) =>{
+                if(error){
+                    res.status(400).json(error)
+                }else{
+                    res.status(201).json(result)
+                }    
+            });
+        }
+
     }
 }
 
